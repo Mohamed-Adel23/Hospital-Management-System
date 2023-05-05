6d05cd650d187c405b1bcfb67f2e80b33200e12c
Mostafa <gguuhhidj@gmail.com>
2023-05-05 04:42:11 +0300

    Mostafa Final Edit Contact-Services-personaldata

--- HMSproject2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ HMSproject2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -78,7 +78,7 @@ namespace HMSproject.Areas.Identity.Pages.Account.Manage
             
             [Required]
             [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [RegularExpression("^[a-zA-Z]*$",ErrorMessage = "The {0} field must contain only characters.")]
+            [RegularExpression("^[a-zA-Z ]+$",ErrorMessage = "The {0} field must contain only characters.")]
             [Display(Name = "Full Name")]
             public string? Name { get; set; }
             
